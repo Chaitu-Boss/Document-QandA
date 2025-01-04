d3b60e896ee27992ebccc06b892f0f8ff5d92fdd
Chaitu-Boss <chaituraut13@gmail.com>
2025-01-05 03:09:06 +0530

    final app.py for streamlit depoyment

---
+++ app.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain.document_loaders import PyPDFLoader
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+
+load_dotenv()
+
+llm=ChatGroq(model="llama3-8b-8192",api_key=st.secrets["GROQ_API_KEY"])
+hf_api_key = st.secrets["huggingface"]["HF_API_KEY"]
+os.environ['HF_TOKEN'] = hf_api_key
+prompt=ChatPromptTemplate.from_template(
+   """
+    You are a helpful assistant who answers question asked by user from his document
+    You are also given a context of document
+    {context}
+    Question: {input}
+    """
+    
+)
+def create_vector_embedding(uploaded_pdf):
+    try:
+        if "vectors" not in st.session_state:
+            with open("temp_uploaded_file.pdf", "wb") as f:
+                f.write(uploaded_pdf.getbuffer())
+            st.session_state.embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+            loader=PyPDFLoader("temp_uploaded_file.pdf") 
+            st.session_state.docs=loader.load()
+            if not st.session_state.docs:
+                st.error("No content found in the uploaded PDF.")
+                return
+            text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+            st.session_state.final_documents=text_splitter.split_documents(st.session_state.docs)
+            if not st.session_state.final_documents:
+                st.error("Not enought content found in the PDF.")
+                return
+            st.session_state.vectors = FAISS.from_documents(
+                st.session_state.final_documents, st.session_state.embeddings
+            )
+            st.success("Vector database created successfully!")
+    except Exception as e:
+        st.error(f"Internal Error: {e}")
+    
+st.title("RAG Document Q&A With Groq And Ollama")
+
+uploaded_pdf=st.file_uploader("Upload your PDF here",type="pdf")
+if uploaded_pdf is not None:
+    if st.button("Process PDF and Create Vector Database"):
+        create_vector_embedding(uploaded_pdf)
+
+user_prompt=st.text_input("Enter your question here")
+
+import time
+
+if user_prompt and "vectors" in st.session_state:
+    document_chain=create_stuff_documents_chain(llm,prompt)
+    retriever=st.session_state.vectors.as_retriever()
+    retriever_chain=create_retrieval_chain(retriever,document_chain)
+    response=retriever_chain.invoke({'input':user_prompt})
+    start=time.process_time()
+    st.write(response['answer'])
+    st.write(f"Time Taken : {time.process_time()-start} seconds")
+
+temp_file = "temp_uploaded_file.pdf"
+if os.path.exists(temp_file):
+    os.remove(temp_file)
+    print(f"Temporary file {temp_file} has been removed.")
+else:
+    print(f"Temporary file {temp_file} does not exist.")
+
+
+    
